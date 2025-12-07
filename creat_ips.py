423b91f3b9bce645bc6d50bbd3ee3396f6d04b74
EitanRubin <noizzhack8@gmail.com>
2025-12-07 12:48:38 +0200

    name ip

--- creat_ips.py
+++ creat_ips.py
@@ -4,4 +4,4 @@ with open("ips.csv", "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
     r = [n for n in range(0, 256)]
     for x in r:
-        writer.writerows([[f'252.25.2.{x}']])
+        writer.writerows([[f'10.0.1.{x}']])
